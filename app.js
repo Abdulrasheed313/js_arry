220660d3a9f1db838ef1dceee548ef5ecc8ec1fd
Abdulrasheed313 <rasheedmumna@gmail.com>
2025-02-02 18:02:52 +0500

    first array

---
+++ app.js
@@ -0,0 +1,199 @@
+// var student = [];
+//  student["ali","aamir","Hassaan"];
+
+
+// var name = ["aamir"];
+
+// var num = [2,3];
+
+// var bul = [true , false];
+
+// var mix = ["rasheed", 0345, true,];
+
+// var mobile = ["jazz","ufone","telenor"];
+
+
+// var edu = ["SSC", "HSC", "BCS", "BS", 'BCOM', "MS",
+// "M. Phil", "PhD"]
+
+
+// document.write(`
+//     1) ${edu[0]} <br>
+//     2) ${edu[1]} <br>
+//     3) ${edu[2]} <br>
+//     4) ${edu[3]} <br>
+//     5) ${edu[4]} <br>
+//     6) ${edu[5]} <br>
+//     7) ${edu[6]} <br>
+//     8) ${edu[7]} <br>
+//     9) ${edu[8]} <br>
+    
+//     `)
+
+
+// var movie = ['Avengers','spectir','jurassic','Inside'];
+
+
+// document.write(`
+//    <h1>Top Movies of 2015</h1> <br>
+//     1) ${movie[0]} <br>
+//     2) ${movie[1]} <br>
+//    3) ${movie[2]} <br>
+//      4) ${movie[3]} <br>
+// Lenght of Array: ${movie.length}
+//`)
+
+
+// var car = ["toyota","Suzuki","Kia","Honda" ];
+// document.write(`
+//    First index of the array 0 <br>
+//    Car at first index of the array${car[0]} <br>
+
+//     Second index of the array 1 <br>
+//    Car at Second index of the array${car[1]} <br>
+
+//    Three index of the array 2 <br>
+//    Car at Three index of the array${car[2]} <br>
+
+//    Four index of the array 3 <br>
+//    Car at Three index of the array${car[3]} <br>
+    
+    
+    
+//     `)
+
+
+
+// var student1 = [
+
+//     ["Micheal" , 320 , 64],
+//     ["Jhon" , 320 , 46],
+//     ["Tony" , 480, 96],
+
+// ] ;
+
+
+// document.write(
+// `Score of ${student1[0][0]} is ${student1[0][1]}. percentage: ${student1[0][2]}  <br>
+// Score of ${student1[1][0]} is ${student1[1][1]}. percentage: ${student1[1][2]}  <br> 
+// Score of ${student1[2][0]} is ${student1[2][1]}. percentage: ${student1[2][2]}  <br>  `
+
+
+// )
+
+
+// var student1 = ["Micheal","Jhon" ,"Tony"];
+
+// var studentScore = [320,230,480];
+
+// document.write(`
+    
+//     Score of ${student1[0]} is ${studentScore[0]}. percentage: ${ studentScore[0] * 100 / 500  } %  <br>
+//     Score of ${student1[1]} is ${studentScore[1]}. percentage: ${ studentScore[1] * 100 / 500  } % <br>
+//     Score of ${student1[2]} is ${studentScore[2]}. percentage: ${ studentScore[2] * 100 / 500  } % <br>
+    
+    
+//     `)
+
+
+// var color = ["Red", "Yellow", "Green"];
+
+
+// document.write(color);
+
+
+// var add = prompt("Add Your Colour Beginning");
+// color.unshift(add);
+
+
+// var end = prompt("add your color in end")
+
+// color.push(end);
+
+// document.write(`<br> b) ${color}`);
+
+// color.unshift("lightGreen","Light Blue")
+
+// document.write(`<br> c) ${color}`);
+
+// color.shift();
+
+// document.write(`<br> d) ${color}`);
+
+// color.pop();
+
+// document.write(`<br> e) ${color}`);
+
+
+
+// var studentScore = [320,230,480,120]
+// document.write(`Sorted student scores: ${studentScore} <br>` );
+// studentScore.sort()
+
+// document.write(`orderd Sorted student scores: ${studentScore} <br> ` );
+
+// var fruitlist = ["strwberry","apple","orange","banana"]
+// document.write(`Fruita List: ${fruitlist} <br>` );
+// fruitlist.sort()
+
+// document.write(`orderd Fruita List:: ${fruitlist} ` );
+
+
+// var citylist = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"];
+
+// document.write(`City List <br> ${citylist}<br>`)
+
+
+// var selectedList = citylist.slice(2, 4);
+
+// document.write(`Selected List <br> ${selectedList}<br>`)
+
+// var arr = ["This ", " is ", "my ", "cat"];
+
+// document.write(arr)
+
+
+
+// var joen = arr.join("");
+
+// document.write(`<br> ${joen}`)
+
+
+
+// var phonemenu = ["Apple",
+//     "Samsung", "Motorola", "Nokia", "Sony","Haier"]
+
+
+//     document.write(`
+//         <select>
+//         <option disabled> Select</option>
+//          <option selected>${phonemenu[0]}</option>
+//          <option>${phonemenu[1]}</option>
+//          <option>${phonemenu[2]}</option>
+//          <option>${phonemenu[3]}</option>
+//          <option>${phonemenu[4]}</option>
+//          <option>${phonemenu[5]}</option>
+        
+        
+//         </select>
+        
+        
+        
+//         `)
+
+
+var multi = [
+
+    [0,1,2,4],
+    [1,0,1,2],
+    [2,1,0,1],
+
+];
+
+document.write(`
+    ${multi[0]} <br>
+    ${multi[1]}<br>
+    ${multi[2]}
+    
+    
+    `)
